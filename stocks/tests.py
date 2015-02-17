382eccc7783476b69cb48ac56a57878d023cbc96
Andrew Wu <andrewmorro@gmail.com>
2015-02-17 23:57:37 +0800

    Generate stock list json Import json to db(loaddata)

--- stocks/tests.py
+++ stocks/tests.py
@@ -2,18 +2,32 @@ from django.test import TestCase
 
 from stocks.models import Stock
 
-import xlrd
+import json
 
 class StockTestCase(TestCase):
     def setUp(self):
+        data = []
         print("Setting up Stock tests...") 
         fp = open('stocks/assets/stocklist.csv')
+        count = 0
         for line in fp:
             raw = line.split(',')
             if len(raw)>1:
-                print(raw)
-                Stock.objects.create(name=raw[1], ticker=raw[0].rstrip())
+                count += 1
+                Stock.objects.create(name=raw[1].rstrip(), ticker=raw[0].rstrip()).save()
+                obj = {
+                    "model": "stocks.Stock",
+                    "pk": count,
+                    "fields": {
+                        "name": raw[1].rstrip(),
+                        "ticker": raw[0].rstrip()
+                    }
+                }
+                data.append(obj)
         fp.close()
+
+        with open('stocks/fixtures/stocklist.json', 'w', encoding='utf-8') as outfile:
+            json.dump(data, outfile)
 
     def test_stock(self):
         print("Test stock...")
