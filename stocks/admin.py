471869706e2aef3c2a161a39f0f9d961b3f086bd
Andrew Wu <andrewmorro@gmail.com>
2015-02-28 15:38:52 +0800

    Generate random stock Add timer

--- stocks/admin.py
+++ stocks/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from stocks.models import Stock,Tag,StockTagRel
 
 # Register your models here.
+admin.site.register(Stock)
+admin.site.register(Tag)
+admin.site.register(StockTagRel)
