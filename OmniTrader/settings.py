f7956433b3901861ff85fa041aa923d28c200f39
Andrew Wu <morro.program@gmail.com>
2015-04-02 22:32:52 +0800

    add prod and mysql config 	modified:   OmniTrader/__init__.py 	modified:   OmniTrader/settings.py 	modified:   OmniTrader/settings_prod.py

--- OmniTrader/settings.py
+++ OmniTrader/settings.py
@@ -62,13 +62,13 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-    'aliyun': {
-        'ENGINE': 'django.db.backends.mysql',
-        'USER': 'omnitraderq',
-        'PASSWORD': 'omnitraderq',
-        'HOST': 'cragguidebook.mysql.rds.aliyuncs.com',
-        'PORT': '3306'
-    }
+#    'aliyun': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'USER': 'omnitraderq',
+#        'PASSWORD': 'omnitraderq',
+#        'HOST': 'cragguidebook.mysql.rds.aliyuncs.com',
+#        'PORT': '3306'
+#    }
 }
 
 # Internationalization
