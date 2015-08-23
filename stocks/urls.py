b5d3e14587ddc3728712b40609253f9b2b167af8
Andrew Wu <andrewmorro@gmail.com>
2015-08-23 13:45:39 +0800

    add getdaydata web service

--- stocks/urls.py
+++ stocks/urls.py
@@ -1,9 +1,10 @@
-from django.conf.urls import patterns, url
+﻿from django.conf.urls import patterns, url
 from stocks import views
 
 urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),
                        url(r'^(?P<stock_ticker>[0-9]+)/$', views.detail, name='detail'),
                        url(r'^(?P<stock_ticker>[0-9]+)/attachtag/$', views.attachtag, name='attachtag'),
+                       url(r'^(?P<stock_ticker>[0-9]+)/daydata/$', views.getDayData, name='getDayData'),
                        url(r'^tags/(?P<tag_id>[0-9]+)/$', views.tags, name='tags'),
                        )
--- stocks/views.py
+++ stocks/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+﻿from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse, Http404
-from .models import Stock, Tag
+from .models import Stock, Tag, DayData
+from django.http.response import JsonResponse
+import json
+from django.core import serializers
 
 
 def index(request):
@@ -43,5 +46,14 @@ def attachtag(request, stock_ticker):
         return render(request, 'stocks/detail.html', {'stock': stock})
     else:
         return render(request, 'stocks/attachtag.html', {'stock': stock})
+    
+def getDayData(request, stock_ticker):
+    try:
+        stock = Stock.objects.get(ticker=stock_ticker)
+        data = serializers.serialize('json',DayData.objects.filter(stock=stock))
+        return JsonResponse({'data':data})
+
+    except Stock.DoesNotExist:
+        raise Http404("Stock does not exist")
     
         
