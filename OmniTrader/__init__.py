a6928116ea058d996683900370a2a1ce5a11662b
Andrew Wu <andrewmorro@gmail.com>
2015-11-28 15:46:07 +0800

    link to dropbox at initiation

--- OmniTrader/__init__.py
+++ OmniTrader/__init__.py
@@ -1,4 +1,14 @@
-import pymysql
+﻿import pymysql
+import dropbox
+import logging
+
+logger = logging.getLogger('stocks.management.readDropbox')
+
+logger.info('Initiating OmniTrader')
 
 #load pymysql as mysqldb - mysqldb does not work with python 3 as ConfigParser is renamed
 pymysql.install_as_MySQLdb()
+
+
+logger.info("Linking to Dropbox account...")
+dbx = dropbox.Dropbox('DO8936TNbkgAAAAAAAAAg4zB4LdJK2SBNBPdjaTWM5mpzjU8dFmp1MV5DNiEXDzk')
