61e171806ff45f5fab8ae3295e349ca5005f046e
Andrew Wu <andrewmorro@gmail.com>
2015-11-28 13:44:35 +0800

    change default env to prod

--- OmniTrader/wsgi.py
+++ OmniTrader/wsgi.py
@@ -1,4 +1,4 @@
-"""
+﻿"""
 WSGI config for OmniTrader project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "OmniTrader.settings_uat")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "OmniTrader.settings_prod")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
