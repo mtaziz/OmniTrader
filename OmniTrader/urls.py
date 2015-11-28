35a6f6dc43e296edf1529f3078bdd75caceda339
Andrew Wu <andrewmorro@gmail.com>
2015-11-28 16:06:56 +0800

    move initialization code to urls.py to give it a try

--- OmniTrader/urls.py
+++ OmniTrader/urls.py
@@ -2,6 +2,11 @@
 from django.contrib import admin
 from stocks import views
 
+import dropbox
+import logging
+
+
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'OmniTrader.views.home', name='home'),
@@ -12,3 +17,13 @@ urlpatterns = patterns('',
     url(r'^stocks/', include('stocks.urls')),
     url(r'^autocomplete/', include('autocomplete_light.urls')),
 )
+
+
+
+
+logger = logging.getLogger('stocks.urls')
+
+logger.info('Initiating OmniTrader')
+
+logger.info("Linking to Dropbox account...")
+dbx = dropbox.Dropbox('DO8936TNbkgAAAAAAAAAg4zB4LdJK2SBNBPdjaTWM5mpzjU8dFmp1MV5DNiEXDzk')
