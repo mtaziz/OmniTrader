c62018a9e20757c46c3a862bbac82a2eb0d27edb
Andrew Wu <andrewmorro@gmail.com>
2017-04-16 00:47:57 +0800

    Implement THS concept crawler

--- stocks/utils/ThsCrawler.py
+++ stocks/utils/ThsCrawler.py
@@ -1,30 +1,27 @@
 import scrapy
-import logging
-import sys
-from scrapy.crawler import CrawlerProcess
-
+from stocks.models import *
+from scrapy.http import Request
+import taggit
 
 class ThsCrawler(scrapy.Spider):
-
-
     name = "ThsCrawler"
+    allowed_domains = ["10jqka.com.cn"]
     start_urls = [
         "http://stockpage.10jqka.com.cn/600340/",
     ]
+
+    def parseStock(self,response):
+        #Trim the url to get the stock code
+        ticker = response.url[31:-1]
+        stock = Stock.objects.get(ticker=ticker)
+        tagList = response.xpath('//dl[@class="company_details"]/dd')[1].xpath('@title').extract()[0].split('，')
+
+        #self.logger.info(tagList)
+        stock.tags.set(*tagList)
+
     def parse(self,response):
-        self.logger.info("world")
+        stocks = Stock.objects.all()
 
-        content = response.xpath('//dl[@class="company_details"]/dd')[1].xpath('@title').extract()
-        self.logger.info(content)
-
-
-
-crawler = ThsCrawler()
-crawler.logger.setLevel(logging.INFO)
-crawler.logger.info("before")
-process = CrawlerProcess()
-
-process.crawl(ThsCrawler)
-process.start() # the script will block here until the crawling is finished
-
-crawler.logger.info("after")
+        for stock in stocks:
+            url = "http://stockpage.10jqka.com.cn/{}/".format(stock.ticker)
+            yield Request(url, callback = self.parseStock)
