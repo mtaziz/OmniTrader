08626865fb0b50273b784d2018b1a2644410f18c
Andrew Wu <andrewmorro@gmail.com>
2017-04-16 23:26:48 +0800

    add location to tag

--- doc/ProdEnvironment.md
+++ doc/ProdEnvironment.md
@@ -1,16 +1,20 @@
 # Prod environment
 Prod environment is now on Aliyun. The runtime environment is python3.4. Always use the following to do python command on prod machine:
 
+Server: 45.78.14.30
+application root: /opt/OmniTrader
+
 # Command line tool
 > $ python3.4 [args]
 > $ sudo pip3.4 install [package name]
 
-## How to import initial data
+# Stock data
+Stock data are kept updated via cron job that triggers django command syncStockList
+Import initial data from a json file just in case:
 > $ python3.4 migrate loaddata all-stocks
-
 You can also change 'all-stocks' to other json fixtures at stocks/fixtures
 
-
-# Filenames are shown as \uxxx\uxxx
+# Troubleshooting
+## Filenames are shown as \uxxx\uxxx
 http://stackoverflow.com/questions/20923663/unicodeencodeerror-ascii-codec-cant-encode-character-in-position-0-ordinal
 try PYTHONIOENCODING=utf-8 on prod-box(morro-bwg) so that the filename will be displayed in Chinese in putty console
--- stocks/utils/ThsCrawler.py
+++ stocks/utils/ThsCrawler.py
@@ -1,6 +1,7 @@
 import scrapy
 from stocks.models import *
 from scrapy.http import Request
+import logging
 import taggit
 
 class ThsCrawler(scrapy.Spider):
@@ -9,16 +10,21 @@ class ThsCrawler(scrapy.Spider):
     start_urls = [
         "http://stockpage.10jqka.com.cn/600340/",
     ]
+    def ThsCrawler(self):
+        self.logger.setLevel(logging.INFO)
 
     def parseStock(self,response):
         #Trim the url to get the stock code
         ticker = response.url[31:-1]
         stock = Stock.objects.get(ticker=ticker)
+        content = response.xpath('//dl[@class="company_details"]/dd')
+        location = content[0].xpath('text()').extract()[0]
+        #self.logger.info(location)
         tagList = response.xpath('//dl[@class="company_details"]/dd')[1].xpath('@title').extract()[0].split('，')
 
         #TODO: change set tag operations to incremental update to facilitate reporting
         #self.logger.info(tagList)
-        stock.tags.set(*tagList)
+        stock.tags.set(location, *tagList)
 
     def parse(self,response):
         stocks = Stock.objects.all()
