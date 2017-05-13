954f84b45263dad68cfe204961917fbc01198fab
Andrew Wu <andrewmorro@gmail.com>
2017-05-13 23:42:04 +0800

    1. Bug fix - now the url param and GET param could work through the same view 2. tag delimiters enhancement - space, comma and chinese comma

--- stocks/urls.py
+++ stocks/urls.py
@@ -6,8 +6,8 @@ from stocks.views.StockViews import StockByTicker,StockByTags
 
 urlpatterns = patterns('',
                        url('^(?P<ticker>[0-9]+)/$', StockByTicker.as_view()),
-                       url('^tags/$', StockByTags.as_view()),
                        url('^tags/(?P<tags>.+)/$', StockByTags.as_view()),
+                       url('^tags/$', StockByTags.as_view()),
                        url(r'^(?P<stock_ticker>[0-9]+)/daydata/$', views.dayData, name='dataData'),
 #                       url(r'^tags/(?P<tag_id>[0-9]+)/$', views.tags, name='tags'),
                        url(r'^trades/(?P<date>[0-9]{4}-[0-9]{2}-[0-9]{2})/$', views.trades, name='trades'),
--- stocks/views/StockViews.py
+++ stocks/views/StockViews.py
@@ -1,8 +1,8 @@
 
 from stocks.models import Stock
-from rest_framework import generics, serializers,status
-from rest_framework.response import Response
+from rest_framework import generics, serializers
 from taggit.models import Tag
+import re
 
 
 class TagSerializer(serializers.ModelSerializer):
@@ -35,13 +35,21 @@ class StockByTags(generics.ListAPIView):
     serializer_class = StockSerializer
 
     def get_queryset(self):
-        if 'tags' not in self.request.query_params or self.request.query_params['tags'] == '':
-            return
+        tags = None
+        if 'tags' not in self.kwargs or self.kwargs['tags'] == '':
+            if 'tags' not in self.request.query_params or self.request.query_params['tags'] == '':
+                return Stock.objects.none()
+            else:
+                tags = self.request.query_params['tags']
+        else:
+            tags = self.kwargs['tags']
         # Split Chinese and English comma - replace Chinese comma with English comma first.
-        tags = self.request.query_params['tags'].replace('，',',').split(',')
+        tags = re.split(r'\s+|,+|，+', tags)
         queryset = None
+        #TODO: Optimize the query so that it does the search in one shot.(Currently depending on tag num)
         for tag in tags:
             if queryset is None:
                 queryset = Stock.objects
             queryset = queryset.filter(tags__name__icontains=tag).distinct()
+
         return queryset
